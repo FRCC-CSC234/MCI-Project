8e735e96969902adcbe80c247c57a59aa21c9d55
Trel Johnson <tjohnson445@student.cccs.edu>
2020-04-29 16:33:30 -0600

    Added circular draw method, commented out Added powerup facts

--- MCI Game/MCI Game/GraphicsMethods.h
+++ MCI Game/MCI Game/GraphicsMethods.h
@@ -3,6 +3,9 @@
 #include "Driver.h"
 //written by Trel J
 
-void drawFlatScreen( vector<Brick> bricks, vector<Ball> balls, Paddle paddle, Level background );
-void drawCircularScreen( std::vector<Brick> bricks, std::vector<Ball> balls, Paddle paddle, Level background ); // To be built at a later time
+void drawFlatScreen( vector<Brick> bricks, vector<Ball> balls, Paddle paddle, Level background, int powerupID=-1 );
+//void drawCircularScreen( std::vector<CBrick> bricks, std::vector<Ball> balls, CPaddle paddle, Level background, int powerupID=-1 ); // To be built at a later time
 void drawTitleScreen( );
+
+// commented out circular screen to avoid errors caused by not having circular brick and paddle types in existance.
+																			// I don't know how to spell	 ^
